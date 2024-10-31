0748efbc9078ee44e2e44e535e5d6d4e4d85ba81
zhoupeijun <zhoupeijun012@163.com>
2024-10-31 09:19:21 +0800

    2323

--- index.js
+++ index.js
@@ -5,25 +5,26 @@ const stockSpider = require(global.TOOL.resolvePath("spider/stock-spider"));
 const schedule = require('node-schedule');
 
 async function exec() {
-    // const date = global.DAYJS().format('YYYY-MM-DD');
-    // if(!(global.CHINESEDAY.isWorkday(date) && !global.CHINESEDAY.isInLieu(date))){
-    //     return
-    // }
-    // try {
-    //     await Info.createTable();
-    //     await Info.resetStatus();
-    //     // 首先更新股票列表
-    //     await stockSpider.updateStockList();
-    //     // 更新股票K线
-    //     await stockSpider.updateStockHistory();
+    const date = global.DAYJS().format('YYYY-MM-DD');
+    if(!(global.CHINESEDAY.isWorkday(date) && !global.CHINESEDAY.isInLieu(date))){
+        return
+    }
+    try {
+        await Info.createTable();
+        await Info.resetStatus();
+        // 首先更新股票列表
+        await stockSpider.updateStockList();
+        // 更新股票K线
+        await stockSpider.updateStockHistory();
 
-    //     // 更新基金列表
+        // 更新基金列表
 
-    //     // 更新基金历史
+        // 更新基金历史
 
-    // } catch(error){
-
-    // }
+    } catch(error){
+        console.log(error);
+    }
 } 
+exec();
 // 当前时间的秒值为 下午3点时执行任务，如：2018-7-8 13:25:10
-schedule.scheduleJob('0 0 15 * * *', exec);
+// schedule.scheduleJob('0 0 15 * * *', exec);
